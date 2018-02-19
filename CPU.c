d56d3ea6454efa16dd9af25913dbc1806a16ef17
skvagell <31451634+skvagell@users.noreply.github.com>
2018-02-19 13:11:23 -0500

    Add files via upload

--- CPU.c
+++ CPU.c
@@ -11,12 +11,17 @@
 #include "CPU.h"
 
 #define IF1 0
+// IF1 load buffer, contains info about instruction currently in IF1
 #define IF2 1
+ //IF1-IF2 buffer, contains info about instruction currently in IF2
 #define ID 2
+//IF2-ID buffer, contains info about instruction currently in ID
 #define EX 3
+//ID-EX buffer, contains info about instruction currently in EX
 #define MEM1 4
-#define MEM2 5
-#define WB 5
+//EX-MEM1 buffer, contains info about instruction currently in Mem1
+#define MEM2 5 //MEM1-MEM2 buffer, contains info about instruction currently in Mem2
+#define WB 6 //MEM2-WB buffer, contains info about instruction currently in WB
 
 struct hash_entry {
     unsigned int address;
@@ -30,23 +35,31 @@ void branch_table_init(struct hash_entry table[BRANCH_TABLE_SIZE]);
 void branch_table_taken(struct hash_entry table[BRANCH_TABLE_SIZE], unsigned int address, int prediction_method);
 void branch_table_not_taken(struct hash_entry table[BRANCH_TABLE_SIZE], unsigned int address, int prediction_method);
 int branch_table_predict(struct hash_entry table[BRANCH_TABLE_SIZE], unsigned int address, int prediction_method);
-void stall(struct trace_item* buffers, int stallPosition);
-void flush(struct trace_item* buffers);
-void advance(struct trace_item* buffers);
-int structural_hazard(struct trace_item* buffers);
-int data_hazard_one(struct trace_item* buffers);
-int data_hazard_two(struct trace_item* buffers);
-void print_trace(int trace_view_on, int cycle_number, struct trace_item *tr_entry);
-int get_next_instr(struct trace_item *tr_entry);
-void init_buffers(struct trace_item* buffers);
+void stall(struct trace_item *buffers, struct trace_item NO_OP, int stallPosition);
+void flush(struct trace_item *buffers, struct trace_item NO_OP, struct trace_item *queue, int *queue_size);
+void advance(struct trace_item *buffers);
+int structural_hazard(struct trace_item *buffers);
+int data_hazard_one(struct trace_item *buffers);
+int data_hazard_two(struct trace_item *buffers);
+void print_trace(int trace_view_on, int cycle_number, struct trace_item *tr_entry, int stage);
+int get_next_instr(struct trace_item **tr_entry, struct trace_item NO_OP, struct trace_item *queue, int *queue_size);
+void init_buffers(struct trace_item *buffers);
+int is_empty(struct trace_item *buffers);
 
 int main(int argc, char **argv)
 {
   struct trace_item buffers[7];
+  struct trace_item queue[3];
+
+  struct trace_item *tr_entry;
+
+  struct trace_item NO_OP;
+  NO_OP.type = ti_NOP;
 
   struct hash_entry branch_table[BRANCH_TABLE_SIZE];
 
-  size_t size;
+  int trace_ended = 0;
+  int queue_size = 0;
   char *trace_file_name;
   int trace_view_on = 0;
   int prediction_method = 0;
@@ -82,13 +95,13 @@ int main(int argc, char **argv)
   trace_init();
 
   while(1) {
-    /* TODO: The propper end condition here should be "When no next instruction, and last read instruction removed from WB "*/
-    size = get_next_instr(&(buffers[IF1]));
-    if (!size) {       /* no more instructions (trace_items) to simulate */
+    /* Move new instruction into the pipeline */
+    if (trace_ended && is_empty(buffers)) {  /* no more instructions (trace_items) to simulate and the pipeline is empty */
       printf("+ Simulation terminates at cycle : %u\n", cycle_number);
       break;
     }
     else {              /* parse the next instruction to simulate */
+      int control_hazard = 0;
       cycle_number++;
 
       /* Handle possible control hazard first */
@@ -106,33 +119,53 @@ int main(int argc, char **argv)
         }
 
         /* Check if control hazard happened */
-        if (prediction != was_taken) {
-          /* Flush */
-          flush(buffers);
-        }
+        control_hazard = prediction != was_taken;
       }
 
-//      if(structural_hazard(buffers)){
-//      	stall(buffers, EX);
-//      }
+      if (control_hazard) {
+        if (trace_view_on) {
+          printf("Control hazard, flush\n");
+        }
+        flush(buffers, NO_OP, queue, &queue_size);
+      }
+
+      else if (structural_hazard(buffers)) {
+        if (trace_view_on) {
+          printf("Structural hazard, stall\n");
+        }
+      	stall(buffers, NO_OP, EX);
+      }
 //
-//      else if(data_hazard_one(buffers)){
-//      	stall(buffers, MEM1);
-//      }
+      else if(data_hazard_one(buffers)) {
+        if (trace_view_on) {
+          printf("Data hazard 1, stall\n");
+        }
+      	stall(buffers, NO_OP, MEM1);
+      }
 //
-//      else if(data_hazard_two(buffers)){
-//		    stall(buffers, MEM2);
-//      }
+      else if(data_hazard_two(buffers)) {
+        if (trace_view_on) {
+          printf("Data hazard 2, stall\n");
+        }
+		    stall(buffers, NO_OP, MEM2);
+      }
 //
-//      else{
-//      	advance(buffers);
-//      }
+      else {
+        if (trace_view_on) {
+          printf("No hazards, advance\n");
+        }
+      	advance(buffers);
+        trace_ended = !get_next_instr(&tr_entry, NO_OP, queue, &queue_size);
+        buffers[IF1] = *tr_entry;
+      }
 //
 //
     }
 
-    /* TODO: print all stages, not only executed one */
-    print_trace(trace_view_on, cycle_number, &(buffers[EX]));
+    int i;
+    for(i = IF1; i <= WB ; i++){
+    	print_trace(trace_view_on, cycle_number, &(buffers[i]), i);
+    }
   }
 
   trace_uninit();
@@ -140,7 +173,7 @@ int main(int argc, char **argv)
   exit(0);
 }
 
-void init_buffers(struct trace_item* buffers) {
+void init_buffers(struct trace_item *buffers) {
   int i = 0;
   for (i = IF1; i <= WB; i++) {
     buffers[i].type = ti_NOP;
@@ -163,9 +196,12 @@ void branch_table_taken(struct hash_entry table[BRANCH_TABLE_SIZE], unsigned int
 
     /* Check if this address isn't present in the table */
     if (table[index].address != address) {
-      table[index].address = table[index].address;
+      table[index].address = address;
       table[index].value = 0;
     }
+
+    /* Increase value */
+    table[index].value++;
 
     /* Limit value */
     if (table[index].value > limit) {
@@ -179,7 +215,7 @@ void branch_table_not_taken(struct hash_entry table[BRANCH_TABLE_SIZE], unsigned
 
   /* Check if this address isn't present in the table */
   if (table[index].address != address) {
-    table[index].address = table[index].address;
+    table[index].address = address;
     table[index].value = 0;
   }
 
@@ -212,90 +248,91 @@ int branch_table_predict(struct hash_entry table[BRANCH_TABLE_SIZE], unsigned in
   return 0;
 }
 
-void stall(struct trace_item* buffers, int stallPosition){
-	/*for(int i = 6; i > stallPosition; i--){
+void stall(struct trace_item *buffers, struct trace_item NO_OP, int stallPosition){
+	int i;
+	//Advance everything ahead of the stall position
+	for(i = WB; i > stallPosition; i--){
 		buffers[i] = buffers[i-1];
 	}
+	//Inject a no op and leave everything behind it where it was
 	buffers[stallPosition] = NO_OP;
-	*/
 }
 
-void flush(struct trace_item* buffers){
-/*for(int i = WB; i > EX; i--){
+void flush(struct trace_item *buffers, struct trace_item NO_OP, struct trace_item *queue, int *queue_size){
+	int i;
+	//Advance everything ahead of the branch/jump
+	for(i = WB; i >= EX; i--){
 		buffers[i] = buffers[i-1];
 	}
+	//Inject a no op
 	buffers[EX] = NO_OP;
-*/
-  /* TODO: IF1, IF2, and ID should be saved in some queue to be not lost and "reintroduced" later in get_next_instr */
-}
-
-void advance(struct trace_item* buffers){
-	/*for(int i = WB; i > IF1; i--){
-		buffers[i] = buffers[i-1];
+	//Squash the instructions behind the branch/jump
+	for(i = IF1; i < EX; i++){
+		queue[(*queue_size)++] = buffers[i];
+    buffers[i] = NO_OP;
 	}
 }
-*/
+
+void advance(struct trace_item *buffers){
+	int i;
+	for(i = WB; i > IF1; i--){
+		buffers[i] = buffers[i-1];
+	}
   /* Don't need to read buffers[IF1] here, it's done in the main loop */
 }
 
-int structural_hazard(struct trace_item* buffers){
-  /* TODO: Re-check hazard conditions */
-	if(((buffers[WB].dReg == buffers[ID].sReg_a) || buffers[ID].sReg_b) && (buffers[WB].type != ti_JTYPE && buffers[WB].type != ti_BRANCH && buffers[WB].type != ti_STORE && buffers[WB].type != ti_JRTYPE)){
+
+int structural_hazard(struct trace_item *buffers){
+	if((buffers[WB].type == ti_RTYPE || buffers[WB].type == ti_ITYPE || buffers[WB].type == ti_LOAD) &&(buffers[WB].dReg == buffers[ID].sReg_a || buffers[WB].dReg == buffers[ID].sReg_b)){
 		return 1;
 	}
 	return 0;
 }
 
-int data_hazard_one(struct trace_item* buffers){
+int data_hazard_one(struct trace_item *buffers){
 	if((buffers[MEM1].type == ti_LOAD) && ((buffers[MEM1].dReg == buffers[EX].sReg_a) || buffers[MEM1].dReg == buffers[EX].sReg_b)){
 		return 1;
 	}
 	return 0;
 }
 
-int data_hazard_two(struct trace_item* buffers){
+int data_hazard_two(struct trace_item *buffers){
 	if((buffers[MEM2].type == ti_LOAD) && ((buffers[MEM2].dReg == buffers[EX].sReg_a) || (buffers[MEM2].dReg == buffers[EX].sReg_b))){
 		return 1;
 	}
 	return 0;
 }
 
-void print_trace(int trace_view_on, int cycle_number, struct trace_item *tr_entry) {
+void print_trace(int trace_view_on, int cycle_number, struct trace_item *tr_entry, int stage) {
   if (trace_view_on) {/* print the executed instruction if trace_view_on=1 */
-    switch(tr_entry->type) {
+    printf("[cycle %d] [stage %d] ", cycle_number, stage);
+    switch (tr_entry->type) {
       case ti_NOP:
-        printf("[cycle %d] NOP\n:",cycle_number) ;
+        printf("NOP\n") ;
         break;
       case ti_RTYPE:
-        printf("[cycle %d] RTYPE:",cycle_number) ;
-        printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(dReg: %d) \n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->dReg);
+        printf("RTYPE: (PC: %x)(sReg_a: %d)(sReg_b: %d)(dReg: %d) \n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->dReg);
         break;
       case ti_ITYPE:
-        printf("[cycle %d] ITYPE:",cycle_number) ;
-        printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->dReg, tr_entry->Addr);
+        printf("ITYPE: (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->dReg, tr_entry->Addr);
         break;
       case ti_LOAD:
-        printf("[cycle %d] LOAD:",cycle_number) ;
-        printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->dReg, tr_entry->Addr);
+        printf("LOAD: (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->dReg, tr_entry->Addr);
         break;
       case ti_STORE:
-        printf("[cycle %d] STORE:",cycle_number) ;
-        printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->Addr);
+        printf("STORE: (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->Addr);
         break;
       case ti_BRANCH:
-        printf("[cycle %d] BRANCH:",cycle_number) ;
-        printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->Addr);
+        printf("BRANCH: (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->Addr);
         break;
       case ti_JTYPE:
-        printf("[cycle %d] JTYPE:",cycle_number) ;
-        printf(" (PC: %x)(addr: %x)\n", tr_entry->PC,tr_entry->Addr);
+        printf("JTYPE: (PC: %x)(addr: %x)\n", tr_entry->PC,tr_entry->Addr);
         break;
       case ti_SPECIAL:
-        printf("[cycle %d] SPECIAL:\n",cycle_number) ;
+        printf("SPECIAL\n") ;
         break;
       case ti_JRTYPE:
-        printf("[cycle %d] JRTYPE:",cycle_number) ;
-        printf(" (PC: %x) (sReg_a: %d)(addr: %x)\n", tr_entry->PC, tr_entry->dReg, tr_entry->Addr);
+        printf("RTYPE: (PC: %x) (sReg_a: %d)(addr: %x)\n", tr_entry->PC, tr_entry->dReg, tr_entry->Addr);
         break;
     }
   }
@@ -303,6 +340,29 @@ void print_trace(int trace_view_on, int cycle_number, struct trace_item *tr_entr
 
 /* Returns 1 and sets instruction info when next instruction exists
    Returns 0 if not */
-int get_next_instr(struct trace_item *tr_entry) {
-  return trace_get_item(&tr_entry);
+int get_next_instr(struct trace_item **tr_entry, struct trace_item NO_OP, struct trace_item *queue, int *queue_size) {
+  /* Firstly search in queue for the next instruction */
+  if ((*queue_size) > 0)
+  {
+    (*queue_size)--;
+    *tr_entry = &(queue[*queue_size]);
+  }
+  /* Then try to take instruction from the trace */
+  else if (!trace_get_item(tr_entry))
+  {
+    /* There are no instruction in the trace, return a no op */
+    *tr_entry = &NO_OP;
+    return 0;
+  }
+  return 1;
+}
+
+int is_empty(struct trace_item *buffers){
+	int i;
+	for(i = 0; i < 6; i++){
+		if(buffers[i].type != ti_NOP){
+			return 0;
+		}
+	}
+	return 1;
 }
